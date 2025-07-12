447f1796283447b501fcc17ea5d6458b790bb175
ersoumyadeepsarkar <ersoumyadeepsarkar@gmail.com>
2025-07-13 02:11:31 +0530

    formated tic tac toe

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -10,12 +10,11 @@ let win_combination = [
 ];
 let cubes = document.querySelectorAll(".btn");
 let reset_b = document.querySelector(".reset");
-let div_container = document.querySelector(".div_container");
-
+let container = document.querySelector(".container");
+let turn = document.getElementById("player");
 let flag = false;
 let isdDraw = false;
-
-function display() {
+let last_player = function display() {
   console.log("display");
   for (let x of win_combination) {
     console.log(x);
@@ -23,59 +22,58 @@ function display() {
     console.log(x[1]);
     console.log(x[2]);
   }
-}
+};
 
 let reset = function () {
   console.log("reset is called");
-  for (let c of cubes) {
+  for (let c of cubes) {D
     c.innerText = "";
     c.classList.remove("O");
     c.classList.remove("X");
     c.removeAttribute("disabled");
-    console.log(c);
     flag = false;
-    count=0;
+    count = 0;
+    c.style.backgroundColor = "";
   }
   let win_msg = document.querySelector("#win_msg");
   if (win_msg) {
     win_msg.innerText = "";
   }
+  turn.innerText = "";
 };
 
 reset_b.addEventListener("click", reset);
 let win_msg = document.createElement("h1");
 win_msg.setAttribute("id", "win_msg");
-win_msg.style.color = "RED";
+win_msg.style.color = "yellow";
 
-let count=0;
+let count = 0;
 let verify = function () {
-    count++;
+  count++;
   for (let comb of win_combination) {
     if (
       cubes[comb[0]]?.innerText &&
       cubes[comb[1]]?.innerText &&
       cubes[comb[2]]?.innerText
     ) {
-        console.log("elgible for verification")
-        console.log("#1 "+cubes[comb[0]].innerText)
-        console.log("#2 "+cubes[comb[1]].innerText)
-        console.log("#3 "+cubes[comb[2]].innerText)
-        
-
       if (
         cubes[comb[0]].innerText === cubes[comb[1]].innerText &&
         cubes[comb[1]].innerText === cubes[comb[2]].innerText
       ) {
-        console.log("we got a winner");
-        win_msg.innerText = "we got a winner";
+        cubes[comb[0]].style.backgroundColor = "yellow";
+        cubes[comb[1]].style.backgroundColor = "yellow";
+        cubes[comb[2]].style.backgroundColor = "yellow";
+
+        console.log(last_player, " is  winner!!");
+        win_msg.innerText = last_player + " is  winner!!";
         document.body.childNodes[0].before(win_msg);
         freeze();
         break;
-      }else{
-       if(count==9){
-            win_msg.innerText = "DRAW!!!";
-
-       }
+      } else {
+        if (count === 9) {
+          win_msg.innerText = "DRAW!!!";
+          console.log("MATCH DRAW!! "+count);
+        }
       }
     }
   }
@@ -83,15 +81,19 @@ let verify = function () {
 
 function play(cube) {
   if (!flag) {
+    last_player = "player #1(X)";
     flag = true;
     cube.innerText = "X";
     cube.classList.add("X");
     cube.setAttribute("disabled", true);
+    turn.innerText = "TURN::player #2(O)";
   } else {
+    last_player = "player #2(O)";
     flag = false;
     cube.innerText = "O";
     cube.classList.add("O");
     cube.setAttribute("disabled", true);
+    turn.innerText = "TURN::player #1(X)";
   }
   verify();
 }
