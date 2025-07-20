b145fbfd526049c92df850601c6b1143ac2c8e83
ersoumyadeepsarkar <ersoumyadeepsarkar@gmail.com>
2025-07-20 06:20:55 +0530

    code un commented

--- apna-college/asynchronus/js/script.js
+++ apna-college/asynchronus/js/script.js
@@ -1,187 +1,187 @@
 // @ts-check
 
-// console.log("init");
-// function hello() {
-//   console.log("helllo every one 1");
-// }
-// /**
-//  *
-//  * SET TIME OUT
-//  *
-//  */
+console.log("init");
+function hello() {
+  console.log("helllo every one 1");
+}
+/**
+ *
+ * SET TIME OUT
+ *
+ */
 
-// setTimeout(hello, 2000);
-// setTimeout(() => {
-//   console.log("hello by arrow function 2");
-// }, 2000);
+setTimeout(hello, 2000);
+setTimeout(() => {
+  console.log("hello by arrow function 2");
+}, 2000);
 
-// console.log("complete");
+console.log("complete");
 
-// /**
-//  *
-//  * CALLBACK EXAMPLE
-//  *
-//  */
+/**
+ *
+ * CALLBACK EXAMPLE
+ *
+ */
 
-// function sum(a, b) {
-//   console.log("sum of a and b" + (a + b));
-// }
+function sum(a, b) {
+  console.log("sum of a and b" + (a + b));
+}
 
-// function calculator(a, b, sumCallback) {
-//   sumCallback(a, b);
-// }
+function calculator(a, b, sumCallback) {
+  sumCallback(a, b);
+}
 
-// calculator(10, 12, sum);
-// calculator(10, 12, (a, b) => {
-//   console.log("sum of a and b" + (a + b));
-// });
+calculator(10, 12, sum);
+calculator(10, 12, (a, b) => {
+  console.log("sum of a and b" + (a + b));
+});
 
-// /**
-//  *
-//  * CALLBACK HELL
-//  *
-//  */
+/**
+ *
+ * CALLBACK HELL
+ *
+ */
 
-// function getData(dataId) {
-//   setTimeout(() => {
-//     console.log("data", dataId);
-//   }, 2000);
-// }
+function getData(dataId) {
+  setTimeout(() => {
+    console.log("data", dataId);
+  }, 2000);
+}
 
-// function getData1(dataId, getNextData) {
-//   setTimeout(() => {
-//     console.log("data1", dataId);
-//     if (getNextData) {
-//       getNextData();
-//     }
-//   }, 2000);
-// }
+function getData1(dataId, getNextData) {
+  setTimeout(() => {
+    console.log("data1", dataId);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
+}
 
-// /**
-//  * sequential
-//  *
-//  */
+/**
+ * sequential
+ *
+ */
 
-// getData(1); // all timeer will start at once
-// getData(2); // all timeer will start at once
-// getData(3); // all timeer will start at once
+getData(1); // all timeer will start at once
+getData(2); // all timeer will start at once
+getData(3); // all timeer will start at once
 
-// // Callback hell
-// getData1(1, () => {
-//   getData1(2, () => {
-//     getData1(3);
-//   });
-// });
+// Callback hell
+getData1(1, () => {
+  getData1(2, () => {
+    getData1(3);
+  });
+});
 
-// /**
-//  * promise
-//  *
-//  *
-//  */
+/**
+ * promise
+ *
+ *
+ */
 
-// let promiseReject = new Promise((resolve, reject) => {
-//   console.log("I am a promise");
-//   reject("ERROR some error occured");
-// });
+let promiseReject = new Promise((resolve, reject) => {
+  console.log("I am a promise");
+  reject("ERROR some error occured");
+});
 
-// let promiseResolve = new Promise((resolve, reject) => {
-//   console.log("I am a promise");
-//   resolve("transaction completions");
-// });
+let promiseResolve = new Promise((resolve, reject) => {
+  console.log("I am a promise");
+  resolve("transaction completions");
+});
 
-// console.log("After promice");
+console.log("After promice");
 
-// function getDataP(dataId, getNextData) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (dataId <= 0) {
-//         console.log(" WRONG data1", dataId);
-//         reject("reject");
-//       } else {
-//         console.log("data1", dataId);
-//         resolve("SUCCESS");
-//         if (getNextData) {
-//           getNextData();
-//         }
-//       }
-//     }, 2000);
-//   });
-// }
+function getDataP(dataId, getNextData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (dataId <= 0) {
+        console.log(" WRONG data1", dataId);
+        reject("reject");
+      } else {
+        console.log("data1", dataId);
+        resolve("SUCCESS");
+        if (getNextData) {
+          getNextData();
+        }
+      }
+    }, 2000);
+  });
+}
 
-// let prom = getDataP(1234556);
-// console.log("Promise immidately after method call", prom);
+let prom = getDataP(1234556);
+console.log("Promise immidately after method call", prom);
 
-// setTimeout(() => {
-//   console.log(prom);
-// }, 2000);
+setTimeout(() => {
+  console.log(prom);
+}, 2000);
 
-// prom.then((m) => alert("succesfull" + m));
+prom.then((m) => alert("succesfull" + m));
 
-// prom = getDataP(-111111);
-// setTimeout(() => {
-//   console.log(prom);
-// }, 2000);
+prom = getDataP(-111111);
+setTimeout(() => {
+  console.log(prom);
+}, 2000);
 
-// prom.catch((e) => alert("FAIL " + e));
+prom.catch((e) => alert("FAIL " + e));
 
-// function asycnFunc() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("some data 1");
-//       resolve("success");
-//     }, 4000);
-//   });
-// }
+function asycnFunc() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("some data 1");
+      resolve("success");
+    }, 4000);
+  });
+}
 
-// function asycnFunc2() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("some data 2");
-//       resolve("success");
-//     }, 6000);
-//   });
-// }
-// console.log("featching data 1");
+function asycnFunc2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("some data 2");
+      resolve("success");
+    }, 6000);
+  });
+}
+console.log("featching data 1");
 
-// let p1 = asycnFunc();
-// p1.then((res) => {
-//   console.log("data 1" + res);
-//   console.log("featching data 2");
-//   let p2 = asycnFunc2();
-//   p2.then((res) => {
-//     console.log("data 2" + res);
-//   });
-// });
+let p1 = asycnFunc();
+p1.then((res) => {
+  console.log("data 1" + res);
+  console.log("featching data 2");
+  let p2 = asycnFunc2();
+  p2.then((res) => {
+    console.log("data 2" + res);
+  });
+});
 
-// console.log("=================featching data 1");
-// asycnFunc().then((x) => {
-//   console.log("%data 1" + x);
-//   console.log("=======================featching data 2");
-//   asycnFunc2().then(() => {
-//     console.log("$data 2" + x);
-//   });
-// });
+console.log("=================featching data 1");
+asycnFunc().then((x) => {
+  console.log("%data 1" + x);
+  console.log("=======================featching data 2");
+  asycnFunc2().then(() => {
+    console.log("$data 2" + x);
+  });
+});
 
-// console.log("solving Callback hell by promise")
-// function getDatax(dataId) {
-//   return new Promise((resolve,reject) => {
-//     setTimeout(() => {
-//       console.log("datax======>", dataId);
-//       resolve(dataId);
-//     }, 2000);
-//   });
-// }
+console.log("solving Callback hell by promise")
+function getDatax(dataId) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      console.log("datax======>", dataId);
+      resolve(dataId);
+    }, 2000);
+  });
+}
 
-// getDatax(1).then((result) => {
-//   console.log("xxxxxxxxxxxxxxx Datax"+result)
-//   getDatax(result * 2).then((result) => {
-//     console.log("xxxxxxxxxxxxxxx Datax"+result)
-//     getDatax(result * 2).then((result)=>{
-//       console.log("Final result"+result)
-//     })
+getDatax(1).then((result) => {
+  console.log("xxxxxxxxxxxxxxx Datax"+result)
+  getDatax(result * 2).then((result) => {
+    console.log("xxxxxxxxxxxxxxx Datax"+result)
+    getDatax(result * 2).then((result)=>{
+      console.log("Final result"+result)
+    })
 
-//   });
-// });
+  });
+});
 
 /**
  *
